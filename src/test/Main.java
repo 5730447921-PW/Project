36de151fd3b644948a2a9f7ebf8ae039500ba364
5730402621-PP <beautifut_@hotmail.com>
2015-12-10 23:25:37 +0700

    test

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,13 +1,11 @@
+package test;
 
 public class Main {
 
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("‚ª√‡®§§§§§T T");
+		System.out.println("Test");
+
 	}
 
 }
